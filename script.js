9bcd9815fcfcc089d1ae32476c3788432feb15dc
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-05-06 22:02:52 +0330

    js finish

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
-function showTime(){
-    let myTime=new Date();
-    let hour=myTime.getHours();
-    let min=myTime.getMinutes();
-    let sec=myTime.getSeconds();
-    let day=myTime.getDay();
-    let month=myTime.getMonth();
-   
-    let weekdays=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    let weekDayConverter=weekdays[day];
+function showTime() {
 
-    let months=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-    let monthsConverter=months[month];
+    let myTime = new Date();
+    let hour = myTime.getHours();
+    let min = myTime.getMinutes();
+    let sec = myTime.getSeconds();
+    let day = myTime.getDay();
+    let month = myTime.getMonth();
 
-    document.getElementById("hour").textContent=hour;
-    document.getElementById("minute").textContent=min;
-    document.getElementById("seconds").textContent=sec;
-    document.getElementById("week").textContent=weekDayConverter;
-    document.getElementById("month").textContent=monthsConverter;
-    document.getElementById("day").textContent=myTime.getDate();
-    document.getElementById("year").textContent=myTime.getFullYear();
+
+    let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let weekDayConverter = weekdays[day];
+
+    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    let monthsConverter = months[month];
+
+    
+    if(hour<10){
+        hour="0"+hour;
+  
+      }
+      if(min<10){
+          min="0"+min;
+          
+      }
+      if(sec<10){
+          sec="0"+sec;
+          
+      }
+    document.getElementById("hour").textContent = hour;
+    document.getElementById("minute").textContent = min;
+    document.getElementById("seconds").textContent = sec;
+    document.getElementById("week").textContent = weekDayConverter;
+    document.getElementById("month").textContent = monthsConverter;
+    document.getElementById("day").textContent = myTime.getDate()+",";
+    document.getElementById("year").textContent = myTime.getFullYear();
+
 }
-setInterval(showTime,1000);
+setInterval(showTime, 1000);
 showTime();
