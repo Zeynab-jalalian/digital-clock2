9e21df97ad99a45efea2a068e3209bd2733c5eed
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-05-06 21:33:22 +0330

    js time

--- script.js
+++ script.js
@@ -4,5 +4,9 @@ function showTime(){
     let min=myTime.getMinutes();
     let sec=myTime.getSeconds();
 
-    
+    document.getElementById("hour").textContent=hour;
+    document.getElementById("minute").textContent=min;
+    document.getElementById("seconds").textContent=sec;
 }
+setInterval(showTime,1000);
+showTime()
