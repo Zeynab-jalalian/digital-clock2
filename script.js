b578d392303cf426b241fde13c954640387aafd8
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-05-06 21:44:27 +0330

    js weekDays

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ function showTime(){
     let hour=myTime.getHours();
     let min=myTime.getMinutes();
     let sec=myTime.getSeconds();
+    let day=myTime.getDay();
+   
+    let weekdays=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let weekDayConverter=weekdays[day];
 
     document.getElementById("hour").textContent=hour;
     document.getElementById("minute").textContent=min;
     document.getElementById("seconds").textContent=sec;
+    document.getElementById("week").textContent=weekDayConverter;
 }
 setInterval(showTime,1000);
-showTime()
+showTime();
