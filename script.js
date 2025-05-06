e2eb70d0d72a313e95cc16cfc9aca2232409b27a
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-05-06 21:49:19 +0330

    js months

--- script.js
+++ script.js
@@ -4,14 +4,19 @@ function showTime(){
     let min=myTime.getMinutes();
     let sec=myTime.getSeconds();
     let day=myTime.getDay();
+    let month=myTime.getMonth();
    
     let weekdays=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     let weekDayConverter=weekdays[day];
+
+    let months=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    let monthsConverter=months[month];
 
     document.getElementById("hour").textContent=hour;
     document.getElementById("minute").textContent=min;
     document.getElementById("seconds").textContent=sec;
     document.getElementById("week").textContent=weekDayConverter;
+    document.getElementById("month").textContent=monthsConverter;
 }
 setInterval(showTime,1000);
 showTime();
